59910139515eb55467bd992c2c85434d746a3e48
antonioamgm <antonioamgm178@gmail.com>
2014-03-06 19:02:29 +0100

    Creadas todas la imagenes que faltaban.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+function sendMail(asunto,cuerpo) {
+var link = "mailto:art@lauraalvaro.es"
+         + "?cc="
+         + "&subject="+asunto
+         + "&body="+cuerpo
+;
+window.location.href = link;
+}
