20145f488195cca78c8b08d454b8c622859acc87
antonioamgm <antonioamgm178@gmail.com>
2014-03-07 18:12:46 +0100

    Cambiando formulario y enlaces de imagenes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-function sendMail(asunto,cuerpo) {
-var link = "mailto:art@lauraalvaro.es"
-         + "?cc="
-         + "&subject="+asunto
-         + "&body="+cuerpo
-;
-window.location.href = link;
+function sendMail() {
+	var asunto="Envío de correo desde la página Web lauraalvaro.es";
+	var cuerpo = document.getElementById('contenido');
+	var usuario=document.getElementById('usuario');
+	var correo=document.getElementById('correo');
+	if(limita(100)){	
+	var link = "mailto:art@lauraalvaro.es" 
+	+ "?cc=" 
+	+ "&subject=" + asunto.value 
+	+ "&body=" + cuerpo.value;
+	window.location.href = link;
+	}
 }
+
+function limita(maximoCaracteres){
+	var elemento=document.getElementById('contenido');
+	if(elemento.value.length>maximoCaracteres){
+		return false;
+	}else{
+		return true;
+	}
+}
+
+window.onload=function(){
+	document.getElementById('usuario').focus();
+	document.getElementById('contenido').onclick=sendMail;
+};
