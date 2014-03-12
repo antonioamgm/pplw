fe0f5b67459bd378a22dd36dc145a02712f88f62
antonioamgm <antonioamgm178@gmail.com>
2014-03-12 13:27:10 +0100

    Cambios en formulario y gestión de datos añadido codificación imagenes en base 64js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,40 @@
 //empieza aqui
 var creamail = {
-	//limita el número de caracteres del textarea
+    //limita el número de caracteres del textarea
     limita: function(maxCaracter) {
-        with (document.formcorreo) {
-            if (contenido.value.length > maxCaracter) {
-                return false;
-            } else {
-                return true;
-            }
+        if (document.formcorreo.contenido.value.length > maxCaracter) {
+            return 0;
+        } else {
+            return 1;
         }
-
     },
     //comprueba que el formulario tiene los campos rellenos
     formCheck: function() {
         var passed = false;
         var error = "";
-        debugger;
+        var enviado = this.getCookie("emailenviado");
+//        debugger;
         with (document.formcorreo) {
             //alert(contenido.value);
             if (usuario.value === "") {
                 //alert("El nombre es obligatorio y debe ser texto");
-                error = errorUsuario;
-                error.innerHTML = "El nombre es obligatorio y debe ser texto";
+                // error = errorUsuario;
+                //error.innerHTML = "El nombre es obligatorio y debe ser texto";
                 usuario.focus();
             }
             else if (correo.value === "") {
                 //alert("Por favor tu email quién envia el correo.");
-                error = errorCorreo;
-                error.innerHTML = "El correo electrónico es obligatorio.";
+                // error = errorCorreo;
+                //error.innerHTML = "El correo electrónico es obligatorio.";
                 correo.focus();
             }
-            else if (contenido.value === "" && this.limita(100)) {
+            else if (contenido.value === "" && this.limita(100) === 1) {
                 //alert("Rellena el contenido del correo");
-                error = errorContenido;
-                error.innerHTML = "El contenido del correo es obligatorio.";
+                // error = errorContenido;
+                // error.innerHTML = "El contenido del correo es obligatorio.";
                 contenido.focus();
             }
-            else if (this.checkMultiple() === "") {
+            else if (enviado === "1") {
                 if (confirm("Acabas de mandar un email utilizando este formulario, ¿estás seguro de que quieres mandar otro?")) {
                     this.process();
                     passed = true;
@@ -51,92 +49,98 @@ var creamail = {
     },
     //comprueba si se ha enviado un email
     checkMultiple: function() {
-        if (this.getCookie("emailenviado") === 'true')
-            return true;
+        var vCookie = this.getCookie("emailenviado");
+        if (vCookie === 1)
+            return 1;
         else
-            return false;
+            return 0;
     },
     process: function() {
-        this.setCookie("emailenviado", "true");
+//         debugger;
+        this.setCookie("emailenviado", 1);
         var fmail = "mailto:";
         var sitio = "lauraalvaro.es";
         var mimail = "art";
         var enlaces = {c: "cc=", s: "subject=", i: "?", am: "&", b: "body=", a: "@"};
         var asunto = {su: 'Sugerencias', op: 'Opinión', inf: 'Informe de errores', re: 'Recomendar', enro: 'Enlaces rotos', ot: 'Otros'};
-        debugger;
         with (document.formcorreo) {
-        	this.setCookie("correo",correo.value);
-        	this.setCookie("usuario",usuario.value);
-            if (listasunto.value === asunto.su){           
+            this.setCookie("correo", correo.value);
+            this.setCookie("usuario", usuario.value);
+            if (listasunto.value === asunto.su) {
                 action = fmail//mailto
                         + mimail + enlaces.a + sitio//correo completo
                         + enlaces.i + enlaces.s + asunto.su;
 //else if (Subject.selectedIndex == 2) action = "mailto:vilber72@hotmail.com?subject=Opinión";
-            }else if (listasunto.value === asunto.op){
+            } else if (listasunto.value === asunto.op) {
                 action = fmail//mailto
                         + mimail + enlaces.a + sitio//correo completo
                         + enlaces.i + enlaces.s + asunto.op;
-            }else if (listasunto.value === asunto.inf){            
+            } else if (listasunto.value === asunto.inf) {
                 action = fmail//mailto
                         + mimail + enlaces.a + sitio//correo completo
                         + enlaces.i + enlaces.s + asunto.inf;
-            }else if (listasunto.value ===asunto.re){
+            } else if (listasunto.value === asunto.re) {
                 action = fmail//mailto
                         + mimail + enlaces.a + sitio//correo completo
                         + enlaces.i + enlaces.s + asunto.re;
-            }else if (listasunto.value === asunto.enro){
+            } else if (listasunto.value === asunto.enro) {
                 action = fmail//mailto
                         + mimail + enlaces.a + sitio//correo completo
                         + enlaces.i + enlaces.s + asunto.enro;
-            }else if (listasunto.value === asunto.ot){
+            } else if (listasunto.value === asunto.ot) {
                 action = fmail//mailto
                         + mimail + enlaces.a + sitio//correo completo
                         + enlaces.i + enlaces.s + asunto.ot;
-            }else{
+            } else {
                 action = fmail//mailto
                         + mimail + enlaces.a + sitio//correo completo
                         + enlaces.i + enlaces.s + asunto.ot;
-            } 
+            }
         }
+        document.getElementById('cierraform').click();
     },
-    setCookie: function(cname,cvalue,exdays) {
+    setCookie: function(cname, cvalue, exdays) {
         var d = new Date();
-		d.setTime(d.getTime()+(exdays*24*60*60*1000));
-		var expires = "expires="+d.toGMTString();
-		document.cookie = cname + "=" + cvalue + "; " + expires;
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
     },
     getCookie: function(cname) {
         var name = cname + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0; i<ca.length; i++)
-  		{
-  			var c = ca[i].trim();
-  			if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-		}
-		return "";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++)
+        {
+            var c = ca[i].trim();
+            if (c.indexOf(name) === 0)
+                return c.substring(name.length, c.length);
+        }
+        return "";
     },
-    checkCookie:function (){
-		var user=getCookie("correo");
-		if (user!=""){
-  			alert("Welcome again " + user);
-  		}else{
-  		user = prompt("Please enter your name:","");
-  		if (user!="" && user!=null){
-    		setCookie("username",user,365);
-    		}
-  		}
-	},
-    getName: function() {
-        if (this.getCookie("correo") !== null) {
-            document.formcorreo.correo.value = this.getCookie("correo");
-            document.formcorreo.usuario.value= this.getCookie("usuario");
+    checkCookie: function(nomCookie) {
+        var user = this.getCookie(nomCookie);
+        if (user !== "") {
+            alert("Bienvenido de nuevo " + user);
+        } else {
+            user = prompt("Por favor entra tu nombre:", "");
+            if (user !== "" && user !== null) {
+                setCookie(nomCookie, user, 365);
+            }
         }
     },
-    setName: function() {
+    getName: function() {
+        if (this.getCookie("correo") !== null) {
+            with (document.formcorreo) {
+                correo.value = this.getCookie("correo");
+                usuario.value = this.getCookie("usuario");
+            }
+
+        }
+    },
+    setName: function(nombre) {
         var expdate = new Date();
         expdate.setTime(expdate.getTime() + (24 * 60 * 60 * 1000 * 365));
         var prompt = i = document.formcorreo.usuario.value;
-        setCookie("name", i, expdate);
+        this.setCookie(nombre, i, expdate);
     },
     getInfo: function() {
         var now = new Date();
@@ -149,9 +153,4 @@ var creamail = {
 window.onload = function() {
     creamail.getInfo();
     creamail.getName();
-
 };
-
-
-
-
